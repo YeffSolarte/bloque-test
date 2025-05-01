6850fd1b11061520e206f27c5de60874338797d7
Yefferson Solarte <yefferson@salespeak.ai>
2025-05-01 12:13:07 -0500

    use preact to reduce build size

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import preact from '@preact/preset-vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    preact(),
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
